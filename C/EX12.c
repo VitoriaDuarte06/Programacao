c1612940551e6b22d84267df2c38d4a432465183
VitoriaDuarte06 <89814272+VitoriaDuarte06@users.noreply.github.com>
2021-08-30 20:53:53 -0300

    Add files via upload

---
+++ C/EX12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main(int argc, char *argv[]){
+  int base, altura, area;
+  
+  printf("Digite o valor da base: ");
+  scanf("%i", &base);
+ 
+  printf("Digite o valor da altura: ");
+  scanf("%i", &altura);
+ 
+  area = base*altura;
+ 
+  if (area > 100){
+      printf("O terreno tem uma area de %i, e um terreno grande!", area);
+  }
+  else{
+      printf ("O terreno possui uma area de %i", area);
+  }
+ 
+  return 0;
+}
