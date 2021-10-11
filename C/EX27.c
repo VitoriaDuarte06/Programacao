188275981dfe9d8ce44dc39659985db45042bf13
VitoriaDuarte06 <89814272+VitoriaDuarte06@users.noreply.github.com>
2021-10-11 16:59:13 -0300

    Add files via upload

---
+++ C/EX27.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int x, a, b, i, t;
+ 
+  printf("Digite um numero: ");
+  scanf("%i", &x);
+ 
+  while(x <= 0)
+  {
+    printf("Erro! Apenas numeros positivos. Digite novamente: ");
+    scanf("%i", &x);
+  }
+  printf("Digite o primeiro valor do intervalo: ");
+  scanf("%i", &a);
+  
+  printf("Digite o segundo valor do intervalo: ");
+  scanf("%i", &b);
+ 
+  while(b < a)
+  {
+    printf("Erro! O segundo valor tem que ser maior que o primeiro. Digite novamente: ");
+    scanf("%i", &b);
+  }
+
+  i=a;
+    while(i <= b){
+    t = x * i;
+    printf("%i x %i = %i\n", x, i, t);
+    i = i + 1;
+  }
+ 
+  return 0;
+}
